45d024daa1d6bcea59246f8018bc11f9226d5f75
Diana <wwwavvves@gmail.com>
2024-07-31 19:07:41 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import Search from "./Search";
 
 function App() {
@@ -6,6 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">Dictionary</header>
       <Search />
+      <footer className="Footer">
+        Coded by{" "}
+        <a href="https://github.com/wwwavvves" target="_blank" rel="noreferrer">
+          Diana Guerreiro
+        </a>{" "}
+        and open-sourced at{" "}
+        <a
+          href="https://github.com/wwwavvves/dictionary_react_addon_shecodes"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
